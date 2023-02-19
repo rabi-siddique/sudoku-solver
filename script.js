52155d62a77f1b08b0de627a0d1df7c29c4065b9
rabi27 <rabisiddique27@gmail.com>
2023-02-19 17:22:09 +0500

    backtracing logic

--- script.js
+++ script.js
@@ -32,4 +32,52 @@ function createGrid() {
   }
 }
 
+function solve(grid) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (grid[i][j] == '.') {
+        for (let number of '123456789') {
+          if (isValid(number, grid, i, j)) {
+            grid[i][j] = number;
+            if (solve(grid)) {
+              return true;
+            }
+            grid[i][j] = '.';
+          }
+        }
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function isValid(value, grid, row, col) {
+  for (let i = 0; i < 9; i++) {
+    if (grid[row][i] == value) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    if (grid[i][col] == value) {
+      return false;
+    }
+  }
+
+  let r = 3 * Math.floor(row / 3);
+  let c = 3 * Math.floor(col / 3);
+
+  for (let i = r; i < r + 3; i++) {
+    for (let j = c; j < c + 3; j++) {
+      if (grid[i][j] == value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
 createGrid();
