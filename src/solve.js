374b912178fdb3e65418d1393d4dbc051467a44e
rabi27 <rabisiddique27@gmail.com>
2023-02-23 11:58:21 +0500

    seperate solver logic into its own module

---
+++ src/solve.js
@@ -0,0 +1,70 @@
+let recursiveCalls = 0;
+const counter = document.getElementById('counter');
+
+async function solve(grid, delay) {
+  recursiveCalls += 1;
+  counter.textContent = `Number of recursive calls: ${recursiveCalls}`;
+  counter.style.backgroundColor = '#333';
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (grid[i][j] == '.') {
+        for (let number of '123456789') {
+          if (isValid(number, grid, i, j)) {
+            grid[i][j] = number;
+            const cell = document
+              .getElementsByClassName('row')
+              [i].getElementsByClassName('cell')[j];
+            cell.innerText = number; // update cell value on screen
+            cell.classList.add('backtracking');
+            await new Promise((resolve) => setTimeout(resolve, delay)); // Add delay
+            if (await solve(grid, delay)) {
+              cell.classList.remove('backtracking');
+              cell.classList.add('solved');
+              return true;
+            }
+            cell.classList.remove('backtracking');
+            grid[i][j] = '.';
+            cell.innerText = ''; // reset cell value on screen
+            await new Promise((resolve) => setTimeout(resolve, delay)); // Add delay
+          }
+        }
+        return false;
+      } else {
+        const cell = document
+          .getElementsByClassName('row')
+          [i].getElementsByClassName('cell')[j];
+        cell.classList.add('backtracking');
+      }
+    }
+  }
+
+  return true;
+}
+
+function isValid(value, grid, row, col) {
+  for (let i = 0; i < 9; i++) {
+    if (grid[row][i] == value) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    if (grid[i][col] == value) {
+      return false;
+    }
+  }
+
+  let r = 3 * Math.floor(row / 3);
+  let c = 3 * Math.floor(col / 3);
+  for (let i = r; i < r + 3; i++) {
+    for (let j = c; j < c + 3; j++) {
+      if (grid[i][j] == value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+export default solve;
