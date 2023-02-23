0f3b7251e2a27bc8eafd9eedadffb00956a35b11
rabi27 <rabisiddique27@gmail.com>
2023-02-23 12:11:27 +0500

    creating slider.js

---
+++ src/slider.js
@@ -0,0 +1,63 @@
+const slider = document.querySelector('.slider');
+const dot = document.querySelector('.dot');
+const value = document.querySelector('.value');
+const progressBar = document.querySelector('.progress-bar');
+
+let delay = 50;
+
+function setDelay(newValue) {
+  delay = newValue;
+  value.textContent = newValue;
+}
+
+function setProgress(value) {
+  progressBar.style.width = value + '%';
+}
+
+function setDotPosition(x) {
+  if (x < 0) {
+    x = 0;
+  } else if (x > slider.offsetWidth - dot.offsetWidth) {
+    x = slider.offsetWidth - dot.offsetWidth;
+  }
+  dot.style.left = x + 'px';
+}
+
+function onMouseMove(event) {
+  let x = event.pageX - slider.getBoundingClientRect().left;
+  x -= dot.offsetWidth / 2;
+  if (x < 0) {
+    x = 0;
+  } else if (x > slider.offsetWidth - dot.offsetWidth) {
+    x = slider.offsetWidth - dot.offsetWidth;
+  }
+  setDotPosition(x);
+  const progress = (x / (slider.offsetWidth - dot.offsetWidth)) * 100;
+  setProgress(progress);
+  setDelay(Math.round(progress));
+}
+
+function onMouseUp() {
+  document.removeEventListener('mousemove', onMouseMove);
+  document.removeEventListener('mouseup', onMouseUp);
+  dot.style.pointerEvents = 'auto';
+}
+
+slider.addEventListener('mousedown', function (event) {
+  event.preventDefault();
+  dot.style.pointerEvents = 'none';
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+dot.addEventListener('mousedown', function (event) {
+  event.preventDefault();
+  dot.style.pointerEvents = 'none';
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+setProgress(delay);
+const initialDotPosition = Math.floor(((delay - 1) / 100) * slider.offsetWidth);
+setDotPosition(initialDotPosition);
+value.textContent = delay;
